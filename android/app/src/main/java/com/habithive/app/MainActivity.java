35a89027d34c6f7b53989a8b34e46d8de3c4732a
Ign22 <ignholdings@gmail.com>
2025-08-06 00:46:33 +0300

    Add manually fixed Capacitor Android project (Gradle files added)

---
+++ android/app/src/main/java/com/habithive/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.habithive.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.habithive.app',
+  appName: 'habithive',
+  webDir: 'dist'
+};
+
+export default config;
